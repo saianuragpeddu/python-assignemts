2626947fd97158060d05ec3ef77e4449a3e29d6c
Sai Abhi peddu <saianuragp@gmail.com>
2019-02-20 13:19:22 -0500

    AWS

--- Session-6/AWS.py
+++ Session-6/AWS.py
@@ -1,7 +1,10 @@
 import boto3
 ec2 = boto3.resource('ec2')
-outfile = open('ec2-keypair.pem','w')
-key_pair = ec2.create_key_pair(KeyName='ec2-kaypair')
+
+outfile = open('ec2-keypair_new.pem','w')
+
+key_pair = ec2.create_key_pair(KeyName='ec2-kaypair_new')
+
 keypairout = str(key_pair.key_material)
 print(keypairout)
 outfile.write(keypairout)
--- Session-6/EC2.py
+++ Session-6/EC2.py
@@ -1,10 +1,16 @@
+# To find the path on which we are working
+import os
+cwd = os.getcwd()
+print('###cwd###', cwd)
+filename = os.path.join(cwd, 'ec2-launch')
+# Create the Ec2 instances
 import boto3
-ec2 = boto3.resource('ec2')
+ec2 = boto3.resource('ec2', region_name = 'us-east-1')
 
 instances = ec2.create_instances(
-    ImageId='ami-04328208f4f0cf1fe',
+    ImageId='ami-035be7bafff33b6b6',
     MinCount=1,
     MaxCount=2,
     InstanceType='t2.micro',
-    KeyName='ec2-keypairpem'
+    KeyName='ec2-launch'
 )
