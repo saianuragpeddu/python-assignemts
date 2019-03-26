bec79c8c07576c79e52fe8d22915374377084590
saianuragpeddu <45405901+saianuragpeddu@users.noreply.github.com>
2019-03-26 16:08:19 -0400

    Update AWSS3Bucket.py

--- Session-6/AWSS3Bucket.py
+++ Session-6/AWSS3Bucket.py
@@ -1,10 +1,14 @@
 import boto3
+
 # Creating S3 Client
 s3 = boto3.client('s3')
+
 # Call for S3 to list current Buckets
 response = s3.list_buckets()
+
 # Get a list of all bucket names from the response
 buckets = [bucket['Name'] for bucket in response['Buckets']]
+
 # Print the bucket list
 print("Bucket List: %s" % buckets)
 
