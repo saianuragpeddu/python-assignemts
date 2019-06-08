4b6936497d7cb17c7206cfe08e7b4d2de94e4edb
Sai Abhi peddu <saianuragp@gmail.com>
2019-06-08 11:52:51 -0400

    Mall_Analysis

---
+++ Session-4-projectskaggle/mallanalysis.py
@@ -0,0 +1,148 @@
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt # plotting
+import numpy as np # linear algebra
+import os # accessing directory structure
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns
+
+print(os.listdir('C:\\Users\\anura\\GitHub\\python-assignments\\Session-4-projectskaggle\\input'))
+
+# Distribution graphs (histogram/bar graph) of column data
+def plotPerColumnDistribution(df, nGraphShown, nGraphPerRow):
+    nunique = df.nunique()
+    df = df[[col for col in df if nunique[col] > 1 and nunique[col] < 50]] # For displaying purposes, pick columns that have between 1 and 50 unique values
+    nRow, nCol = df.shape
+    columnNames = list(df)
+    nGraphRow = (nCol + nGraphPerRow - 1) / nGraphPerRow
+    plt.figure(num = None, figsize = (6 * nGraphPerRow, 8 * nGraphRow), dpi = 80, facecolor = 'w', edgecolor = 'k')
+    for i in range(min(nCol, nGraphShown)):
+        plt.subplot(nGraphRow, nGraphPerRow, i + 1)
+        columnDf = df.iloc[:, i]
+        if (not np.issubdtype(type(columnDf.iloc[0]), np.number)):
+            valueCounts = columnDf.value_counts()
+            valueCounts.plot.bar()
+        else:
+            columnDf.hist()
+        plt.ylabel('counts')
+        plt.xticks(rotation = 90)
+        plt.title(f'{columnNames[i]} (column {i})')
+    plt.tight_layout(pad = 1.0, w_pad = 1.0, h_pad = 1.0)
+    plt.show()
+
+# Correlation matrix
+def plotCorrelationMatrix(df, graphWidth):
+    filename = df.dataframeName
+    df = df.dropna('columns') # drop columns with NaN
+    df = df[[col for col in df if df[col].nunique() > 1]] # keep columns where there are more than 1 unique values
+    if df.shape[1] < 2:
+        print(f'No correlation plots shown: The number of non-NaN or constant columns ({df.shape[1]}) is less than 2')
+        return
+    corr = df.corr()
+    plt.figure(num=None, figsize=(graphWidth, graphWidth), dpi=80, facecolor='w', edgecolor='k')
+    corrMat = plt.matshow(corr, fignum = 1)
+    plt.xticks(range(len(corr.columns)), corr.columns, rotation=90)
+    plt.yticks(range(len(corr.columns)), corr.columns)
+    plt.gca().xaxis.tick_bottom()
+    plt.colorbar(corrMat)
+    plt.title(f'Correlation Matrix for {filename}', fontsize=15)
+    plt.show()
+
+# Scatter and density plots
+def plotScatterMatrix(df, plotSize, textSize):
+    df = df.select_dtypes(include =[np.number]) # keep only numerical columns
+    # Remove rows and columns that would lead to df being singular
+    df = df.dropna('columns')
+    df = df[[col for col in df if df[col].nunique() > 1]] # keep columns where there are more than 1 unique values
+    columnNames = list(df)
+    if len(columnNames) > 10: # reduce the number of columns for matrix inversion of kernel density plots
+        columnNames = columnNames[:10]
+    df = df[columnNames]
+    ax = pd.plotting.scatter_matrix(df, alpha=0.75, figsize=[plotSize, plotSize], diagonal='kde')
+    corrs = df.corr().values
+    for i, j in zip(*plt.np.triu_indices_from(ax, k = 1)):
+        ax[i, j].annotate('Corr. coef = %.3f' % corrs[i, j], (0.8, 0.2), xycoords='axes fraction', ha='center', va='center', size=textSize)
+    plt.suptitle('Scatter and Density Plot')
+    plt.show()
+
+nRowsRead = 1000 # specify 'None' if want to read whole file
+# Mall_Customers.csv has 27820 rows in reality, but we are only loading/previewing the first 1000 rows
+df1 = pd.read_csv('C:\\Users\\anura\\GitHub\\python-assignments\\Session-4-projectskaggle\\input\\Mall_Customers.csv', delimiter=',', nrows = nRowsRead)
+df1.dataframeName = 'Mall_Customers.csv'
+nRow, nCol = df1.shape
+print(f'There are {nRow} rows and {nCol} columns')
+
+# The first 5 rows in the csv file.
+print(df1.head(5))
+
+# The last 5 rows in the CSV file.
+print(df1.tail(5))
+
+# Describe the data
+print(df1.describe())
+
+# Data types in the data
+print(df1.dtypes)
+
+# See any null in the Data
+print(df1.isnull().sum())
+
+# Distribution Graph
+plotPerColumnDistribution(df1, 10, 5)
+
+# Correlation Matrix
+plotCorrelationMatrix(df1, 8)
+
+# scatter and density plots
+plotScatterMatrix(df1, 20, 10)
+
+# Data visulation
+plt.style.use('fivethirtyeight')
+
+# Histogram on Age, Annual Income and Spending Score
+plt.figure(1 , figsize = (15 , 6))
+n = 0
+for x in ['Age' , 'Annual Income (k$)' , 'Spending Score (1-100)']:
+    n += 1
+    plt.subplot(1 , 3 , n)
+    plt.subplots_adjust(hspace =0.5 , wspace = 0.5)
+    sns.distplot(df1[x] , bins = 20)
+    plt.title('Distplot of {}'.format(x))
+plt.show()
+
+# Count Plot of Gender
+plt.figure(1 , figsize = (15 , 5))
+sns.countplot(y = 'Gender' , data = df1)
+plt.show()
+
+# Ploting the Relation between Age, Annual Income and Spending Score
+plt.figure(1 , figsize = (15 , 7))
+n = 0
+for x in ['Age' , 'Annual Income (k$)' , 'Spending Score (1-100)']:
+    for y in ['Age' , 'Annual Income (k$)' , 'Spending Score (1-100)']:
+        n += 1
+        plt.subplot(3 , 3 , n)
+        plt.subplots_adjust(hspace = 0.5 , wspace = 0.5)
+        sns.regplot(x = x , y = y , data = df1)
+        plt.ylabel(y.split()[0]+' '+y.split()[1] if len(y.split()) > 1 else y )
+plt.show()
+
+# Age vs Annual Income w.r.t Gender
+plt.figure(1 , figsize = (15 , 6))
+for gender in ['Male' , 'Female']:
+    plt.scatter(x = 'Age' , y = 'Annual Income (k$)' , data = df1[df1['Gender'] == gender] ,
+                s = 200 , alpha = 0.5 , label = gender)
+plt.xlabel('Age'), plt.ylabel('Annual Income (k$)')
+plt.title('Age vs Annual Income w.r.t Gender')
+plt.legend()
+plt.show()
+
+# Annual income vs Spending Score w.r.t gender
+plt.figure(1 , figsize = (15 , 6))
+for gender in ['Male' , 'Female']:
+    plt.scatter(x = 'Annual Income (k$)',y = 'Spending Score (1-100)' ,
+                data = df1[df1['Gender'] == gender] ,s = 200 , alpha = 0.5 , label = gender)
+plt.xlabel('Annual Income (k$)'), plt.ylabel('Spending Score (1-100)')
+plt.title('Annual Income vs Spending Score w.r.t Gender')
+plt.legend()
+plt.show()
